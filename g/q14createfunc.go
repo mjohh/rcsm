1da1248bd807c29b66f2b900986833e4392310a3
root <root@dev08.badinfo.rcs>
2016-09-28 16:10:11 +0800

    edit answer

--- g/q14createfunc.go
+++ g/q14createfunc.go
@@ -14,10 +14,19 @@ func plusX() func (a int, x int) int{
     }
 }
 
+func plusX2(x int) func (a int) int{
+    return func (a int) int{
+        return a+x
+    }
+}
+
 func main(){
     f := plusTwo()
     fmt.Println(3,f(3))
 
     f2 := plusX()
     fmt.Println(3,6,f2(3,6))
+
+    f3 := plusX2(6)
+    fmt.Println(3,f3(3))
 }
