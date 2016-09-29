4841c6fc4814cbbd1c5d5656fa5a4e1594818b46
root <root@dev08.badinfo.rcs>
2016-09-29 09:12:27 +0800

    fix bubble sort bug

--- g/q13bubblesort.go
+++ g/q13bubblesort.go
@@ -4,16 +4,10 @@ import "fmt"
 func bubblesort(a []int){
     len := len(a)
     for i:=0; i<len-1; i++{
-        // for high efficiency
-        swapped := false
         for j:=i+1; j<len; j++{
             if a[i] > a[j]{
                 a[i],a[j] = a[j],a[i]
-                swapped = true
             }
-        }
-        if swapped == false{
-            break;
         }
     }
 }
@@ -24,6 +18,11 @@ func main() {
 
     bubblesort(a)
     fmt.Println(a)
+
+    bubblesort(a)
+    fmt.Println(a)
+
+    a = []int{1,2,9,7,6,5,4,3}
 
     bubblesort(a)
     fmt.Println(a)
