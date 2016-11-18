6dfd9212e00d7a25c13ba2d220d4ff548d03ea13
root <root@dev08.badinfo.rcs>
2016-11-18 11:09:59 +0800

    begin simu go sched

---
+++ src/goschedsimu.c
@@ -0,0 +1,18 @@
+#include "stdio.h"
+typedef struct G G;
+typedef struct M M;
+typedef struct P P;
+
+struct G {
+    long gid; 
+    M* m; 
+};
+
+struct M {
+    long mid;
+    
+}
+
+int main(){
+    printf("\nhello world!");
+}
